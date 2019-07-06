bd9d4d1a07491880ea2373128d9780e760f7e1b4
armand chariot <armand@chariot.be>
2019-07-06 19:12:31 +0200

    first commit - add alamofire cocoaPods

---
+++ pixel-city/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pixel-city
+//
+//  Created by Armand Chariot on 06/07/2019.
+//  Copyright © 2019 Armand Chariot. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
