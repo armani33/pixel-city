55dff01eeca3b8e4fdf5118189438e09b0920be3
armand chariot <armand@chariot.be>
2019-07-07 18:22:50 +0200

    build the user interface of the map, create the basic in MapVC, and MapKit

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+import MapKit
 
 class MapVC: UIViewController {
-
+    
+    //Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
 
-
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
+    }
+    
 }
 
+extension MapVC: MKMapViewDelegate {
+    
+}
